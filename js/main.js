6d71ee307ef31a6432e721eef67839031911d15e
dariavorom <dariavorom@gmail.com>
2020-12-07 13:50:41 +0300

    finished adaptive for index.html

--- js/main.js
+++ js/main.js
@@ -11,9 +11,18 @@ $(function(){
     $('.heroes__slider-img').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        prevArrow: '<button class="slick-btn slick-prev"><img src="images/arrow-left.png" alt="prev"></button>',
-        nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow-right.png" alt="prev"></button>',
-        asNavFor: '.heroes__slider-text'
+        prevArrow: '<button class="slick-btn slick-prev"><img class="arrow-p" src="images/arrow-left.png" alt="prev"></button>',
+        nextArrow: '<button class="slick-btn slick-next"><img class="arrow-n" src="images/arrow-right.png" alt="next"></button>',
+        asNavFor: '.heroes__slider-text',
+        responsive: [
+            {
+                breakpoint: 426,
+                settings: {
+                    arrows: false,
+                    dots: true
+                }
+            }
+        ]
     });
     $('.heroes__slider-text').slick({
         slidesToShow: 1,
